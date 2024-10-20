ac4f5dcff90aa939fce57840acfb6334d1e21316
Priyal Modi <164036980+priyalmodi0904@users.noreply.github.com>
2024-10-20 16:39:03 +0000

    datatypes

---
+++ variable.js
@@ -0,0 +1,9 @@
+const accountID=2332;
+var accountName="priyal";
+let accountnumber=123456;
+let city;
+console.table([accountID,accountName,accountnumber,city]);
+
+// prefer not to use var becouse of issue in block scope and functional scope
+
+//alert("heyy") we are using nodejs not browser
