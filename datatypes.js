c48464f7141219a207cd57a860c1c07c346aaa92
Priyal Modi <164036980+priyalmodi0904@users.noreply.github.com>
2024-10-20 16:38:25 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+//"use strict" by writing this the code will be strictly follows javascript syntax 
+
+//javascript original documentation tc39.es/ecma..... mdn also
+
+
+
+
+//datatypes
+// primitive
+// number
+// BigInt
+// String
+// null=>standalone
+// Boolean
+// undefined
+// Symbol=>for uniqueness
+
+let age=12;
+console.log(typeof(age))
+
+
+// Object=>non primitive
+// type of undefined is undefined
+// and type of null is Object
